a4b73495670243f7948dc2a64aa9e63838a66046
y-eisen <eisenkrein@yandex.ru>
2022-09-15 21:18:37 +0300

    files 1-3

--- WorkWithFiles_SF/Task1_4_unit_8/Program.cs
+++ WorkWithFiles_SF/Task1_4_unit_8/Program.cs
@@ -7,15 +7,45 @@ class mainClass
 
     public static void Main(string[] args)
     {
-        //Console.WriteLine("Какое упражнение будем делать?");
-        //string s = Console.ReadLine();
+        Console.WriteLine("Какое упражнение будем делать (1-4)?");
+        string unitN = Console.ReadLine();
+        //string unitN = "4";
+        DirectoryInfo mainDir = null;
+
+        long[] filesSize=new long[2];
+        long[] filesCounter = new long[2];
+
+        switch (unitN)
+        {
+            case "1":
+                mainDir= Unit_1_8.MainFunction1();
+                break;
+            case "2":
+                Console.WriteLine("\n\nВызвано упражнение 2\n\n");
+                mainDir = Unit_1_8.MainFunction2();
+                break;
+            case "3":
+                Console.WriteLine("\n\nВызвано упражнение 3\n\n");
+                mainDir = Unit_1_8.MainFunction2();
+                filesSize[0] = Math.Abs(Unit_1_8.DirSize(mainDir, false));
+                filesCounter[0] = Math.Abs(Unit_1_8.filesCounter(mainDir, false));
+                mainDir = Unit_1_8.MainFunction1(mainDir);
+                mainDir = Unit_1_8.MainFunction2(mainDir);
+                filesSize[1] = Math.Abs(Unit_1_8.DirSize(mainDir, false));
+                filesCounter[1] = Math.Abs(Unit_1_8.filesCounter(mainDir, false));
+                
+                Console.WriteLine("\nИТОГИ:\nДо чистки папка весила {0} байт и содержала {1} файлов", filesSize[0], filesCounter[0]);
+                if (filesSize[1]>0||filesCounter[1]>0)                
+                    Console.WriteLine("После чистки папка стала весить {0} байт и содержать {1} файлов", filesSize[1], filesCounter[1]);
+                Console.WriteLine("Удалено {0} файлов", filesCounter[0]- filesCounter[1]);
+                Console.WriteLine("Освобождено {0} байт", filesSize[0] - filesSize[1]);
+                break;
+            default:
+                Unit_1_8.MainFunction4();
+                break;
+        }
 
 
-        //Unit_1_8_1.MainFunction1();
-        Unit_1_8_1.mainFunction2();
-
-        
-        
         Console.ReadKey();
     }
 }
@@ -25,14 +55,21 @@ class mainClass
 //======================
 //======================
 
-public class Unit_1_8_1
+public class Unit_1_8
 {
-    public static void MainFunction1()
+    public static DirectoryInfo MainFunction1(params DirectoryInfo[] dirIn)
     {
         byte timeLag = 0;
         bool deleted;
 
-        DirectoryInfo mainD = getFolder();
+        DirectoryInfo mainD;
+        try
+        { mainD = dirIn[0]; }
+        catch
+        {
+            Console.WriteLine("\n\nВызвано упражнение 1\n\n");
+            mainD = getFolder();
+        }
 
         try
         { Console.WriteLine("Имя папки успешно задано: " + mainD.FullName); }
@@ -90,14 +127,45 @@ public class Unit_1_8_1
         }
         Console.WriteLine("\nЗакончили упражнение");
 
+        return mainD;
     }
 
-    public static void mainFunction2()
-    { 
-    
+    public static DirectoryInfo MainFunction2(params DirectoryInfo[] dirIn)
+    {
+        DirectoryInfo mainD;
+        try
+        {   mainD = dirIn[0]; }
+        catch
+        {   //Console.WriteLine("Вызвано упражнение 2\n\n");
+             mainD = getFolder(); }
+
+        try
+        { Console.WriteLine("Имя папки успешно задано: " + mainD.FullName); }
+        catch
+        {   //Console.WriteLine(e.Message);
+            Console.WriteLine("Завершаем работу приложения");
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+
+        long result = DirSize(mainD,true);
+
+        if (result>0)
+        {   Console.WriteLine("\nВес папки {0} составляет {1} байт", mainD, result);}
+        else
+        {   Console.WriteLine("\nВес папки {0} составляет не менее {1} байт", mainD, -result);
+            Console.WriteLine("Папка пуста (если после сообщения об успешном задании имени папки не было сообщений об ошибках), либо в процессе подсчёта встретились какие-то проблемы и тогда подсчёт может быть некорректен");
+        }
+        return mainD;
     }
 
-   //======================
+    public static void MainFunction4(params string[] args)
+    {
+        Console.WriteLine("\n\nВызвано упражнение 4\n\n");
+
+    }
+
+    //======================
     public static DirectoryInfo getFolder()
     {
         Console.WriteLine("Введите путь к папке: ");
@@ -151,5 +219,53 @@ public class Unit_1_8_1
         return result;
     }
     //====================== 
+
+    public static long DirSize(DirectoryInfo dr,bool showMessages)
+    {   long size = 0;
+        bool okFlag=true;
+
+        if (size < 0) {okFlag = false; size = -size; }
+        
+        FileInfo[] fs = dr.GetFiles();
+        foreach (var ff in fs)
+        {   try {size += ff.Length;}
+            catch (Exception e)
+            {   okFlag = false;
+                if (showMessages) Console.WriteLine("Не получается посчитать вес файла {0}, код ошибки {1}", ff.FullName, e.Message);}
+        }
+
+        DirectoryInfo[] ds = dr.GetDirectories();
+        foreach (var dd in ds)
+        {   try { size += DirSize(dd, showMessages); }
+            catch (Exception e)
+            {   okFlag = false;
+                if (showMessages) Console.WriteLine("Ошибка запроса веса папки {0}, код ошибки {1}", dd.FullName, e.Message);            }
+        }
+        if (!okFlag) { size = -size; }
+        return size;
+    }
+
+    public static long filesCounter(DirectoryInfo dr, bool showMessages)
+    {
+        long counter = 0;
+        bool okFlag = true;
+
+        if (counter < 0) { okFlag = false; counter = -counter; }
+
+        FileInfo[] fs = dr.GetFiles();
+        foreach (var ff in fs)
+        {   counter += 1; }
+
+        DirectoryInfo[] ds = dr.GetDirectories();
+        foreach (var dd in ds)
+        {   try { counter += filesCounter(dd, showMessages); }
+            catch (Exception e)
+            {   okFlag = false;
+                if (showMessages) Console.WriteLine("Ошибка запроса количества файлов в папке {0}, код ошибки {1}", dd.FullName, e.Message);
+            }
+        }
+        if (!okFlag) { counter = -counter; }
+        return counter;
+    }
 }
 
